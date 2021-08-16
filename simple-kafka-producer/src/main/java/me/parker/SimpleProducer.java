1f3072fd35d5273ec1eb6b56df5989076a70e448
parker1609 <psbum1609@gmail.com>
2021-08-16 16:04:35 +0900

    브로커 정상 전송 여부 확인 동기/비동기

--- simple-kafka-producer/src/main/java/me/parker/SimpleProducer.java
+++ simple-kafka-producer/src/main/java/me/parker/SimpleProducer.java
@@ -4,41 +4,34 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 public class SimpleProducer {
     private final static String TOPIC_NAME = "test";
     private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Properties configs = new Properties();
         configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        // 커스텀 파티셔너 추가
-        configs.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class);
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
 
         // value 레코드 전송
         String messageValue = "testMessage";
         ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, messageValue);
-        producer.send(record);
-        log.info("{}", record);
+        // sync-callback
+        RecordMetadata metadata = producer.send(record).get();
+        log.info("{}", metadata.toString());
 
-        // key-value 레코드 전송
-        ProducerRecord<String, String> keyRecord = new ProducerRecord<>(TOPIC_NAME, "Pangyo", "23");
-        producer.send(keyRecord);
-        log.info("{}", keyRecord);
-
-        // 특정 파티션에 key-value 레코드 전송
-        ProducerRecord<String, String> exactPartitionRecord = new ProducerRecord<>(TOPIC_NAME, 0, "Park", "30");
-        producer.send(exactPartitionRecord);
-        log.info("{}", exactPartitionRecord);
-
+        // async-callback
+        producer.send(record, new ProducerCallback());
 
         producer.flush();
         producer.close();
