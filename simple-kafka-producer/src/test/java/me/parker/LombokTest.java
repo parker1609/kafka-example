cea9712debff6e63a7f48d597ed8702812e7bf7f
parker1609 <psbum1609@gmail.com>
2021-08-16 12:35:16 +0900

    심플 카프카 프로듀서 서브모듈 생성 및 단일 메시지 보내기 기능 구현

---
+++ simple-kafka-producer/src/test/java/me/parker/LombokTest.java
@@ -0,0 +1,24 @@
+package me.parker;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class LombokTest {
+
+    @Test
+    void name() {
+        Dto dto = new Dto();
+        dto.setA("aaa");
+
+        log.info("{}", dto.getA());
+    }
+
+    @Setter
+    @Getter
+    public static class Dto {
+        private String a;
+    }
+}
