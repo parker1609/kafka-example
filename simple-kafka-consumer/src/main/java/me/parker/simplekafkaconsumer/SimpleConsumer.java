de40a60c861166b15d3a3f4d638a988cf6628467
parker1609 <psbum1609@gmail.com>
2021-08-22 12:23:13 +0900

    리밸런스 리스터를 가진 컨슈머

--- simple-kafka-consumer/src/main/java/me/parker/simplekafkaconsumer/SimpleConsumer.java
+++ simple-kafka-consumer/src/main/java/me/parker/simplekafkaconsumer/SimpleConsumer.java
@@ -2,13 +2,19 @@ package me.parker.simplekafkaconsumer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 @Slf4j
@@ -16,6 +22,9 @@ public class SimpleConsumer {
     private final static String TOPIC_NAME = "test";
     private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
     private final static String GROUP_ID = "test-group";
+
+    private static KafkaConsumer<String, String> consumer;
+    private static Map<TopicPartition, OffsetAndMetadata> currentOffset = new HashMap<>();
 
     public static void main(String[] args) {
         Properties configs = new Properties();
@@ -25,21 +34,31 @@ public class SimpleConsumer {
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
 
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
-        consumer.subscribe(Arrays.asList(TOPIC_NAME));
+        consumer = new KafkaConsumer<>(configs);
+        consumer.subscribe(Arrays.asList(TOPIC_NAME), new RebalanceListener());
 
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
             for (ConsumerRecord<String, String> record : records) {
                 log.info("{}", record);
+                currentOffset.put(new TopicPartition(record.topic(), record.partition()),
+                        new OffsetAndMetadata(record.offset() + 1, null));
+                consumer.commitSync(currentOffset);
             }
-            consumer.commitAsync((offsets, exception) -> {
-                if (exception != null) {
-                    log.error("Commit failed", exception);
-                } else {
-                    log.info("Commit succeeded");
-                }
-            });
+        }
+    }
+
+    private static class RebalanceListener implements ConsumerRebalanceListener {
+
+        @Override
+        public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+            log.warn("Partitions are revoked");
+            consumer.commitSync(currentOffset);
+        }
+
+        @Override
+        public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+            log.warn("Partitions are assigned");
         }
     }
 }
