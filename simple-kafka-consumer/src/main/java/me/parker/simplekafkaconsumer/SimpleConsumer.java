1ae752283e8099a7212c6e505392941687f62bb3
parker1609 <psbum1609@gmail.com>
2021-08-22 12:12:19 +0900

    심플 카프카 컨슈머 동기 커밋

--- simple-kafka-consumer/src/main/java/me/parker/simplekafkaconsumer/SimpleConsumer.java
+++ simple-kafka-consumer/src/main/java/me/parker/simplekafkaconsumer/SimpleConsumer.java
@@ -23,6 +23,7 @@ public class SimpleConsumer {
         configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
         consumer.subscribe(Arrays.asList(TOPIC_NAME));
@@ -32,6 +33,7 @@ public class SimpleConsumer {
             for (ConsumerRecord<String, String> record : records) {
                 log.info("{}", record);
             }
+            consumer.commitSync();
         }
     }
 }
