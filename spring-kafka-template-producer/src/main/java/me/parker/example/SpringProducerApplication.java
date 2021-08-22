ee22394a0b2db830acebc60df91c82bae6c2aa93
parker1609 <psbum1609@gmail.com>
2021-08-22 15:44:12 +0900

    스프링 커스텀 카프카 템플릿 구현

---
+++ spring-kafka-template-producer/src/main/java/me/parker/example/SpringProducerApplication.java
@@ -0,0 +1,42 @@
+package me.parker.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaProducerException;
+import org.springframework.kafka.core.KafkaSendCallback;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+
+@SpringBootApplication
+public class SpringProducerApplication implements CommandLineRunner {
+
+    private final static String TOPIC_NAME = "test";
+
+    @Autowired
+    private KafkaTemplate<String, String> customKafkaTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(SpringProducerApplication.class);
+        application.run(args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        ListenableFuture<SendResult<String, String>> future = customKafkaTemplate.send(TOPIC_NAME, "test");
+        future.addCallback(new KafkaSendCallback<>() {
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+            }
+
+            @Override
+            public void onFailure(KafkaProducerException ex) {
+            }
+        });
+
+        System.exit(0);
+    }
+}
